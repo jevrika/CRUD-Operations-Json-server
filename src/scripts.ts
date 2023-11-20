0ded51a9511ed6cbe2b325646b5c4463cda640bd
Ērika Skubida <erikaskubida23@gmail.com>
2023-11-20 22:24:28 +0200

    Made the input of the book title, author, year and genre, and it all appears in separate cards.

--- src/scripts.ts
+++ src/scripts.ts
@@ -1,8 +1,74 @@
-import $ from 'jquery';
-import sum from './utils/sum/sum';
+import axios from 'axios';
 
-console.log('Ready for coding');
+const bookWrapper = document.querySelector<HTMLDivElement>('.js-book-wrapper');
+const bookForm = document.querySelector<HTMLDivElement>('.js-book-form');
 
-console.log('Body jQuery node:', $('body'));
-console.log('Body javascript node:', document.querySelector('body'));
-console.log('2 + 3 =', sum(2, 3));
+type Book = {
+  id: number;
+  name: string;
+  author: string;
+  genres: string;
+  year: string;
+};
+
+const drawBooks = () => {
+  bookWrapper.innerHTML = '';
+
+  axios.get<Book[]>('http://localhost:3004/books').then((response): void => {
+    response.data.forEach((book): void => {
+      bookWrapper.innerHTML += `
+      <div class="book" >
+      <h1 class="book__heading">${book.name}</h1>
+      <h2 class="book-author__heading">${book.author}</h2>
+      <h3 class="book-genres__heading">${book.genres} </h3>
+      <h4 class="book-year__heading">${book.year} </h4>
+      <button class='js-delete__button book__button' data-book-id='${book.id}'> Delete </button>
+      </div>
+    `;
+    });
+    const bookDeleteButton = document.querySelectorAll<HTMLButtonElement>('.js-delete__button');
+
+    bookDeleteButton.forEach((bookBtn):void => {
+      bookBtn.addEventListener('click', () => {
+        const { bookId } = bookBtn.dataset;
+
+        axios.delete(`http://localhost:3004/books/${bookId}`).then(() => {
+          drawBooks();
+        });
+      });
+    });
+  });
+};
+
+drawBooks();
+
+bookForm.addEventListener('submit', (event) => {
+  event.preventDefault();
+
+  const bookNameInput = bookForm.querySelector<HTMLInputElement>('input[name="book-name"]');
+  const bookNameInputValue = bookNameInput.value;
+
+  const bookAuthorName = bookForm.querySelector<HTMLInputElement>('input[name="author-name"]');
+  const authorNameInputValue = bookAuthorName.value;
+
+  const bookGenresInput = bookForm.querySelector<HTMLInputElement>('input[name="book-genres"]');
+  const bookGenresInputValue = bookGenresInput.value;
+
+  const bookYearInput = bookForm.querySelector<HTMLInputElement>('input[name="book-year"]');
+  const bookYearInputValue = bookYearInput.value;
+
+  axios
+    .post<Book>('http://localhost:3004/books', {
+      name: bookNameInputValue,
+      author: authorNameInputValue,
+      genres: bookGenresInputValue,
+      year: bookYearInputValue,
+    })
+    .then((): void => {
+      bookNameInput.value = '';
+      bookAuthorName.value = '';
+      bookGenresInput.value = '';
+      bookYearInput.value = '';
+    });
+  drawBooks();
+});
