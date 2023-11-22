a983750bab79a71bc386a0e54650a0d65d1e7638
Ērika Skubida <erikaskubida23@gmail.com>
2023-11-22 19:59:56 +0200

    Make a edit option

--- src/scripts.ts
+++ src/scripts.ts
@@ -1,8 +1,17 @@
 import axios from 'axios';
-import { differenceInMinutes } from 'date-fns';
+import { differenceInMinutes, differenceInHours, differenceInDays } from 'date-fns';
 
 const bookWrapper = document.querySelector<HTMLDivElement>('.js-book-wrapper');
+const formWrapper = document.querySelector<HTMLButtonElement>('.form-wrapper');
 const bookForm = document.querySelector<HTMLDivElement>('.js-book-form');
+
+const bookNameInput = bookForm.querySelector<HTMLInputElement>('input[name="book-name"]');
+const bookAuthorName = bookForm.querySelector<HTMLInputElement>('input[name="author-name"]');
+const bookGenresInput = bookForm.querySelector<HTMLInputElement>('select[name="book-genre"]');
+const bookYearInput = bookForm.querySelector<HTMLInputElement>('input[name="book-year"]');
+
+let isEditMode = false;
+let lastSelectedId = '0';
 
 type Book = {
   id: number;
@@ -12,41 +21,75 @@ type Book = {
   year: string;
   createdAt: string;
 };
+// Funkcija, kura izlogo laiku pirms cik ilga laika tika uztaisīta kartiņa.
+const bookCreatedAt = (createdAt: string):string => {
+  const currentDate: Date = new Date();
+  const resultInMinutes: number = differenceInMinutes(currentDate, Date.parse(createdAt));
+  const resultInHours: number = differenceInHours(currentDate, Date.parse(createdAt));
+  const resultInDays: number = differenceInDays(currentDate, Date.parse(createdAt));
 
-const difference = (createdAt: string) => {
-  const currentDate = new Date();
-  const resultInMinutes = differenceInMinutes(currentDate, Date.parse(createdAt));
-
-  let hours = 0;
-  hours += Math.floor(resultInMinutes / 60);
-
-  let days = 0;
-  days += Math.floor(hours / 24);
-
-  if (days === 1 || hours === 24) {
-    return `Created ${days} day ago `;
+  if ((resultInDays as number) === 1 || (resultInHours as number) === 24) {
+    return `Created ${resultInDays} day ago `;
   }
-  if (days > 1 || hours > 24) {
-    return `Created ${days} days ago `;
+  if ((resultInDays as number) > 1 || (resultInHours as number) > 24) {
+    return `Created ${resultInDays} days ago `;
   }
-  if (resultInMinutes === 60 || hours === 1) {
-    return `Created ${hours} hour ago `;
+  if ((resultInMinutes as number) === 60 || (resultInHours as number) === 1) {
+    return `Created ${resultInHours} hour ago `;
   }
-  if (resultInMinutes > 60 && hours > 1) {
-    return `Created ${hours} hours ago `;
+  if ((resultInMinutes as number) > 60 && (resultInHours as number) > 1) {
+    return `Created ${resultInHours} hours ago `;
   }
-  if (resultInMinutes === 0) {
-    return 'Just now';
+  if ((resultInMinutes as number) === 0) {
+    return 'Created Just now';
   }
-  if (resultInMinutes === 1) {
+  if ((resultInMinutes as number) === 1) {
     return `Created ${resultInMinutes} minute ago`;
   }
-  if (resultInMinutes > 1 && resultInMinutes < 60) {
+  if ((resultInMinutes as number) > 1 && (resultInMinutes as number) < 60) {
     return `Created ${resultInMinutes} minutes ago `;
   }
+  return 'Created at an unknown time';
 };
 
-const drawBooks = () => {
+// Funkcija kura dzēš ārā grāmatu
+const deleteBook = () => {
+  const bookDeleteButton = document.querySelectorAll<HTMLButtonElement>('.js-delete__button');
+
+  bookDeleteButton.forEach((bookDeleteBtn) => {
+    bookDeleteBtn.addEventListener('click', () => {
+      const { bookId } = bookDeleteBtn.dataset;
+
+      axios.delete(`http://localhost:3004/books/${bookId}`).then(() => {
+        drawBooks();
+      });
+    });
+  });
+};
+
+// Funkcija, kura edito gramatu
+const editBook = ():void => {
+  const bookEditButton = document.querySelectorAll<HTMLButtonElement>('.js-edit__button');
+  bookEditButton.forEach((bookEditBtn):void => {
+    bookEditBtn.addEventListener('click', async () => {
+      const addButton = document.querySelector<HTMLButtonElement>('.js-add__button');
+      const { bookId } = bookEditBtn.dataset;
+      const axiosResponse = await axios.get(`http://localhost:3004/books/${bookId}`).then((rs) => rs.data);
+
+      bookNameInput.value = axiosResponse.name;
+      bookAuthorName.value = axiosResponse.author;
+      bookGenresInput.value = axiosResponse.genres;
+      bookYearInput.value = axiosResponse.year;
+      addButton.className = 'book-edit__button';
+      addButton.innerText = 'Update';
+      isEditMode = true;
+      lastSelectedId = bookId;
+    });
+  });
+};
+
+// Funkcija, kura uzzīme kartiņas
+const drawBooks = ():void => {
   bookWrapper.innerHTML = '';
 
   axios.get<Book[]>('http://localhost:3004/books').then((response) => {
@@ -60,55 +103,65 @@ const drawBooks = () => {
       <h2 class="book-author__heading">${book.author}</h2>
       <h3 class="book-genres__heading">Genre: ${book.genres} </h3>
       <h4 class="book-year__heading">First published : ${book.year} year </h4>
+      <button class='js-edit__button book-edit__button' data-book-id='${book.id}'> Edit </button>
       <button class='js-delete__button book-delete__button' data-book-id='${book.id}'> Delete </button>
-      <p class="creating-date"> ${difference(book.createdAt)} </p>
+      <p class="creating-date"> ${bookCreatedAt(book.createdAt)} </p>
       </div>
     `;
     });
-    const bookDeleteButton = document.querySelectorAll<HTMLButtonElement>('.js-delete__button');
-
-    bookDeleteButton.forEach((bookBtn): void => {
-      bookBtn.addEventListener('click', () => {
-        const { bookId } = bookBtn.dataset;
-
-        axios.delete(`http://localhost:3004/books/${bookId}`).then(() => {
-          drawBooks();
-        });
-      });
-    });
+    editBook();
+    deleteBook();
   });
 };
 
 drawBooks();
 
+// Eventlistener un kas notiek, kad submito formu
 bookForm.addEventListener('submit', (event) => {
   event.preventDefault();
 
-  const bookNameInput = bookForm.querySelector<HTMLInputElement>('input[name="book-name"]');
   const bookNameInputValue = bookNameInput.value;
-
-  const bookAuthorName = bookForm.querySelector<HTMLInputElement>('input[name="author-name"]');
   const authorNameInputValue = bookAuthorName.value;
-
-  const bookGenresInput = bookForm.querySelector<HTMLInputElement>('select[name="book-genre"]');
   const bookGenresInputValue = bookGenresInput.value;
-
-  const bookYearInput = bookForm.querySelector<HTMLInputElement>('input[name="book-year"]');
   const bookYearInputValue = bookYearInput.value;
+  // ja editmode ir false, tad taisa jaunu grāmatu
+  if (isEditMode === false) {
+    axios
+      .post<Book>('http://localhost:3004/books', {
+        name: bookNameInputValue,
+        author: authorNameInputValue,
+        genres: bookGenresInputValue,
+        year: bookYearInputValue,
+        createdAt: `${new Date()}`,
+      })
+      .then(() => {
+        bookNameInput.value = '';
+        bookAuthorName.value = '';
+        bookGenresInput.value = 'choose a book genre';
+        bookYearInput.value = '';
+      });
+    // ja editmode ir true, tad rediģē esošo
+  } else {
+    const editButton = formWrapper.querySelector<HTMLButtonElement>('.book-edit__button');
+    const bookId = lastSelectedId;
+    axios
+      .patch<Book>(`http://localhost:3004/books/${bookId}`, {
+        name: bookNameInputValue,
+        author: authorNameInputValue,
+        genres: bookGenresInputValue,
+        year: bookYearInputValue,
+        createdAt: `${new Date()}`,
+      })
+      .then(() => {
+        bookNameInput.value = '';
+        bookAuthorName.value = '';
+        bookGenresInput.value = 'choose a book genre';
+        bookYearInput.value = '';
 
-  axios
-    .post<Book>('http://localhost:3004/books', {
-      name: bookNameInputValue,
-      author: authorNameInputValue,
-      genres: bookGenresInputValue,
-      year: bookYearInputValue,
-      createdAt: `${new Date()}`,
-    })
-    .then((): void => {
-      bookNameInput.value = '';
-      bookAuthorName.value = '';
-      bookGenresInput.value = 'choose a book genre';
-      bookYearInput.value = '';
-    });
+        editButton.className = 'js-add__button book-form__button';
+        editButton.innerText = 'Add book!';
+      });
+    isEditMode = false;
+  }
   drawBooks();
 });
